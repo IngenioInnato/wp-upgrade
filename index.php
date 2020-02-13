8918c7ff86f7c5575393e2713f8d8567288d19bb
Miguel Peña <MiguelAngel0201>
2020-02-13 00:19:04 -0400

    Mejoras ayuda wordpress de seguridad implementados

--- index.php
+++ index.php
@@ -56,5 +56,165 @@ add_filter('login_errors', create_function('$a',"return null;"));
   }
 // ============================
 
-/* ========= */
+// Prohibir WP-Admin de suscriptores
+function restrict_access_admin_panel(){
+  global $current_user;
+  get_currentuserinfo();
+  if ($current_user->user_level <  4) {
+    wp_redirect( get_bloginfo('url') );
+    exit;
+  }
+}
+add_action('admin_init', 'restrict_access_admin_panel', 1);
+// =================================
 
+
+// Desactivar XML - RCP
+add_filter( 'xmlrpc_methods', function( $methods ) {
+  unset( $methods['pingback.ping'] );
+  return $methods;
+} );
+// ====================
+
+// Restringir nombres de usuarios comunes
+function sozot_validate_username($valid, $username) {
+  $forbidden = array('admin', 'webmaster', 'Editor', 'Administrador', 'Dios', 'SuperAdmin', 'foro', 'foros', 'moderador', 'Super Administrador', 'Autor', 'Ayuda WordPress');
+  $pages = get_pages();
+  foreach ($pages as $page) {
+      $forbidden[] = $page->post_name;
+  }
+  if(!$valid || is_user_logged_in() && current_user_can('create_users') ) return $valid;
+  $username = strtolower($username);
+  if ($valid && strpos( $username, ' ' ) !== false) $valid=false;
+  if ($valid && in_array( $username, $forbidden )) $valid=false;
+  if ($valid && strlen($username) < 5) $valid=false;
+  return $valid;
+}
+add_filter('validate_username', 'sozot_validate_username', 10, 2);
+
+function sozot_registration_errors($errors) {
+  if ( isset( $errors->errors['invalid_username'] ) )
+      $errors->errors['invalid_username'][0] = __( 'ERROR: Nombre de usuario no válido, elige otro.', 'sozot' );
+  return $errors;
+}
+add_filter('registration_errors', 'sozot_registration_errors');
+// ======================================
+
+
+
+// Limpieza del head
+/**
+ * Limpieza de wp_head()
+ *
+ * Elimina enlaces innecesarios
+ * Elimina el CSS utilizado por el widget de comentarios recientes
+ * Elimina el  CSS utilizado en las galerías
+ * Elimina el cierre automático de etiquetas y cambia de ''s a "'s en rel_canonical()
+ */
+function nowp_head_cleanup() {
+  // Eliminamos lo que sobra de la cabecera
+  remove_action('wp_head', 'rsd_link');
+  remove_action('wp_head', 'wp_generator');
+  remove_action('wp_head', 'feed_links', 2);
+  remove_action('wp_head', 'index_rel_link');
+  remove_action('wp_head', 'wlwmanifest_link');
+  remove_action('wp_head', 'feed_links_extra', 3);
+  remove_action('wp_head', 'start_post_rel_link', 10, 0);
+  remove_action('wp_head', 'parent_post_rel_link', 10, 0);
+  remove_action('wp_head', 'adjacent_posts_rel_link', 10, 0);
+  remove_action('wp_head', 'adjacent_posts_rel_link_wp_head', 10, 0);
+  remove_action('wp_head', 'wp_shortlink_wp_head', 10, 0);
+  remove_action('wp_head', 'feed_links', 2);
+  remove_action('wp_head', 'feed_links_extra', 3);
+  
+  global $wp_widget_factory;
+  remove_action('wp_head', array($wp_widget_factory->widgets['WP_Widget_Recent_Comments'], 'recent_comments_style'));
+  
+  if (!class_exists('WPSEO_Frontend')) {
+    remove_action('wp_head', 'rel_canonical');
+    add_action('wp_head', 'nowp_rel_canonical');
+  }
+}
+function nowp_rel_canonical() {
+  global $wp_the_query;
+  
+  if (!is_singular()) {
+    return;
+  }
+  
+  if (!$id = $wp_the_query->get_queried_object_id()) {
+    return;
+  }
+  
+  $link = get_permalink($id);
+  echo "\t<link rel=\"canonical\" href=\"$link\">\n";
+}
+add_action('init', 'nowp_head_cleanup');
+
+/**
+ * Eliminamos la versión de WordPress
+ */
+add_filter('the_generator', '__return_false');
+
+
+/**
+ * Limpieza de los language_attributes() usados en la etiqueta <html>
+ *
+ * Cambia lang="es-ES" a lang="es"
+ * Elimina dir="ltr"
+ */
+function nowp_language_attributes() {
+  $attributes = array();
+  $output = '';
+  
+  if (function_exists('is_rtl')) {
+    if (is_rtl() == 'rtl') {
+      $attributes[] = 'dir="rtl"';
+    }
+  }
+  
+  $lang = get_bloginfo('language');
+  
+  if ($lang && $lang !== 'es-ES') {
+    $attributes[] = "lang=\"$lang\"";
+  } else {
+    $attributes[] = 'lang="es"';
+  }
+  
+  $output = implode(' ', $attributes);
+  $output = apply_filters('nowp_language_attributes', $output);
+  
+  return $output;
+}
+add_filter('language_attributes', 'nowp_language_attributes');
+// =================
+
+
+// Cambiar URL de autor a id
+function change_author_permalinks() {
+  global $wp_rewrite;
+   // Primero cambiamos el valor de la base de author a lo que queramos
+   $WP_rewrite->author_base = 'perfil';
+  $wp_rewrite->flush_rules();
+}
+ 
+add_action('init','change_author_permalinks');
+ 
+add_filter('query_vars', 'users_query_vars');
+function users_query_vars($vars) {
+    // ahora añadimos el ID de usuario a la lista de variables válidas
+    $new_vars = array('perfil');
+    $vars = $new_vars + $vars;
+    return $vars;
+}
+ 
+//Y finalmente generamos la regla de escritura de la URL con los valores anteriores 
+function user_rewrite_rules( $wp_rewrite ) {
+  $newrules = array();
+  $new_rules['perfil/(\d*)$'] = 'index.php?author=$matches[1]';
+  $wp_rewrite->rules = $new_rules + $wp_rewrite->rules;
+}
+add_filter('generate_rewrite_rules','user_rewrite_rules');
+// =========================
+
+/* ========= */ 
